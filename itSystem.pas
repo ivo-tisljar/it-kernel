e00efb85a035ad8cac6cde4ac0246a31c4bfbf2a
ivo-tisljar <ivo.tisljar@gmail.com>
2022-11-18 22:42:52 +0100

    standardize formating of if-then statements

--- itSystem.pas
+++ itSystem.pas
@@ -41,8 +41,6 @@ procedure OpenFileInDefaultBrowser (const Handle:HWND; const FileName: string);
 procedure OpenFileInEdge (const Handle:HWND; const FileName: string);
 procedure OpenFileInNotepadPlusPlus (const Handle:HWND; const FileName: string);
 
-procedure StripBOMFromUtf8File (const FileName:string);
-
 function  TimeIntToAnsiTimeStr (Time: integer): string;
 
 
@@ -55,7 +53,7 @@ uses
 
 function  AppendBackslash (const DirectoryName: string): string;
 begin
-  if DirectoryName[Length (DirectoryName)] = '\' then
+  if (DirectoryName[Length (DirectoryName)] = '\') then
     Result := DirectoryName
   else
     Result := DirectoryName + '\';
@@ -101,7 +99,7 @@ end;
 
 function IfInt (const Test: boolean; const i1: integer): integer;
 begin
-  if Test then
+  if (Test) then
     Result := i1
   else
     Result := 0;
@@ -112,7 +110,7 @@ end;
 
 function IfInt (const Test: boolean; const i1, i2: integer): integer;
 begin
-  if Test then
+  if (Test) then
     Result := i1
   else
     Result := i2;
@@ -123,7 +121,7 @@ end;
 
 function IfStr (const Test: boolean; const s1: string): string;
 begin
-  if Test then
+  if (Test) then
     Result := s1
   else
     Result := '';
@@ -134,7 +132,7 @@ end;
 
 function IfStr (const Test: boolean; const s1, s2: string): string;
 begin
-  if Test then
+  if (Test) then
     Result := s1
   else
     Result := s2;
@@ -173,54 +171,6 @@ procedure OpenFileInNotepadPlusPlus (const Handle:HWND; const FileName: string);
 begin
   ShellExecute (Handle, 'open', Pchar ('"C:\Program Files (x86)\Notepad++\notepad++.exe"'),
                Pchar ('"' + FileName + '"'), nil, sw_ShowNormal);
-end;
-
-
-// skip BOM header and copy rest of the file, raise exception if no BOM header found
-
-procedure SkipBOMAndCopyRestOfTheFile (var InputFile, TempFile: TFileStream; const FileName: string);
-var
-  FileHeader: AnsiString;
-const
-  Utf8BomHeader: AnsiString = #$EF#$BB#$BF;   // UTF-8 BOM header
-
-begin
-  InputFile.Read(FileHeader, 3);              // read 3 bytes
-
-  if FileHeader <> Utf8BomHeader then
-    raise EitSystemConversionError.Create('Invalid UTF-8 BOM header!' + CrLf + CrLf + 'File: ' + FileName);
-
-  InputFile.Seek(Length(Utf8BomHeader), soFromBeginning);   // move file pointer behind BOM header
-  TempFile := TFileStream.Create(FileName + '.~temp', fmCreate);
-  TempFile.CopyFrom(InputFile, InputFile.Size - Length(Utf8BomHeader));
-end;
-
-
-// remove BOM header from UTF-8 BOM files and makes it a proper UTF-8 file
-
-procedure StripBOMFromUtf8File (const FileName: string);          // file name (directory included)
-var
-  InputFile, TempFile: TFileStream;
-
-begin
-  try
-    InputFile := TFileStream.Create (FileName, fmOpenRead);
-    try
-      SkipBOMAndCopyRestOfTheFile(InputFile, TempFile, FileName); // copy rest of original file to temp
-    finally
-      FreeAndNil (TempFile);
-      FreeAndNil (InputFile);
-    end;
-
-    DeleteFile (FileName);                                        // delete original file
-    RenameFile (FileName + '.~temp', FileName);                   // rename temp file
-  except
-    on E: EitSystemConversionError do
-      raise;
-    on E: Exception do
-      raise EitSystemConversionError.Create ('UTF-8 BOM header removal failed!' + CrLf + CrLf +
-                                             'File: ' + FileName + CrLf + CrLf + E.Message);
-  end;
 end;
 
 
